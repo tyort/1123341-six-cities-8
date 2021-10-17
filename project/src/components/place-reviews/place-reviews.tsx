a9e3168c2ac1942d41fb74c4cff12c9afe387d5f
tyort <neotyo1986@gmail.com>
2021-10-17 15:35:21 +0300

    Количество отзывов

--- project/src/components/place-reviews/place-reviews.tsx
+++ project/src/components/place-reviews/place-reviews.tsx
@@ -14,7 +14,7 @@ function PlaceReviewsScreen(props: ReviewsScreenProps): JSX.Element {
 
   return (
     <section className="property__reviews reviews">
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews__list">
         {reviews.map((review) => (
           <ReviewScreen
