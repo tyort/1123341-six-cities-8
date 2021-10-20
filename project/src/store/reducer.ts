156c82075ef5fe32bef049c1e1f6b2a86ceb9f17
tyort <neotyo1986@gmail.com>
2021-10-20 19:58:52 +0300

    Рефакторинг reducer

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,8 +1,14 @@
 import {ActionsType, ActionName} from '../types/action';
-import {offers, city} from '../mocks/offers';
+import {offers} from '../mocks/offers';
 import {State} from '../types/state';
 
-const initialState = {city, offersList: offers}; // Начальное значение {объект города, массив списка предложений}
+const CITY_DEFAULT = {
+  latitude: 48.86,
+  longitude: 2.35,
+  title: 'Paris',
+  zoom: 12,
+};
+const initialState = {city: CITY_DEFAULT, offersList: offers}; // Начальное значение {объект города, массив списка предложений}
 
 // Возвращает и принимает в качестве стейта === {объект города, массив списка предложений}
 const reducer = (state: State = initialState, action: ActionsType): State => {
