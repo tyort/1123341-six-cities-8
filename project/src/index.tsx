4f8fcd1db16955a3af3f451af1e2a3bca26b767d
tyort <neotyo1986@gmail.com>
2021-10-20 18:53:13 +0300

    Подключаем redux к проекту

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {offers, city} from './mocks/offers';
+import {reducer} from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      offers={offers}
-      city={city}
-    />
+    <Provider store = {store}>
+      <App
+        offers={offers}
+        city={city}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
-
