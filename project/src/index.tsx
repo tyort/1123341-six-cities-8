9749bfd4388c6358feb9edbfd6280e275fcedd75
tyort <neotyo1986@gmail.com>
2021-10-20 19:07:16 +0300

    Добавит поддержку инструмента Redux DevTools.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {offers, city} from './mocks/offers';
 import {reducer} from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <React.StrictMode>
