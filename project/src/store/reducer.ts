5d228179bad568b8ef90092788b51fbcf0af727a
tyort <neotyo1986@gmail.com>
2021-10-22 23:48:36 +0300

    Action - ChangeSortName в reducer

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,6 +1,7 @@
 import {ActionsType, ActionName} from '../types/action';
 import {offers, cities} from '../mocks/offers';
 import {State} from '../types/state';
+import {Offer, City} from '../types/offer';
 
 const SORT_NAME_DEFAULT = 'Popular';
 const CITY_DEFAULT = {
@@ -10,6 +11,20 @@ const CITY_DEFAULT = {
   zoom: 12,
 };
 
+const sortOffers = (proffer: Offer[], sortName: string, city: City): Offer[] => {
+  switch (sortName) {
+    case 'Price: low to high':
+      return proffer.slice().sort((a, b) => a.price - b.price);
+    case 'Price: high to low':
+      return proffer.slice().sort((a, b) => b.price - a.price);
+    case 'Top rated first':
+      return proffer.slice().sort((a, b) => b.rating - a.rating);
+    default:
+      // Чтобы выдать порядок какой был на сервере
+      return offers.filter((item) => item.city === city.title);
+  }
+};
+
 // Начальное значение {объект города, массив списка предложений, название сортировки}
 const initialOffers = offers.filter((item) => item.city === CITY_DEFAULT.title);
 const initialState = {city: CITY_DEFAULT, offersList: initialOffers, sortName: SORT_NAME_DEFAULT};
@@ -17,12 +32,19 @@ const initialState = {city: CITY_DEFAULT, offersList: initialOffers, sortName: S
 //               state: {объект города, массив списка предложений}
 //               action: {type: 'название', payload: переменная с компонента}
 const reducer = (state: State = initialState, action: ActionsType): State => {
-  const city = cities.find((town) => town.title === action.payload) || CITY_DEFAULT;
-  const offersList = offers.filter((item) => item.city === city.title);
-
   switch (action.type) {
     case ActionName.ChangeCity:
-      return {...state, city, offersList};
+      return {
+        ...state,
+        city: cities.find((town) => town.title === action.payload) || CITY_DEFAULT,
+        offersList: offers.filter((item) => item.city === action.payload),
+      };
+    case ActionName.ChangeSortName:
+      return {
+        ...state,
+        sortName: action.payload,
+        offersList: sortOffers(state.offersList, action.payload, state.city),
+      };
     default:
       return state;
   }
