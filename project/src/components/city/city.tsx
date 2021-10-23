cfddf53c17d8a4742bafcbe596096ac0d7667fc8
tyort <neotyo1986@gmail.com>
2021-10-23 21:06:35 +0300

    Рефакторинг компонента city и подключение его, как children

--- project/src/components/city/city.tsx
+++ project/src/components/city/city.tsx
@@ -1,26 +1,33 @@
 import {ChangeCityPayload} from '../../types/action';
+import {City} from '../../types/city';
+import {nanoid} from 'nanoid';
 
 type CityScreenProps = {
-  cityTitle: ChangeCityPayload;
+  cities: City[];
   onCityChoose: (cityTitle: ChangeCityPayload) => void
 }
 
 function CityScreen(props: CityScreenProps): JSX.Element {
-  const {cityTitle, onCityChoose} = props;
+  const {cities, onCityChoose} = props;
 
   return (
-    <li
-      className="locations__item"
-      data-city={cityTitle}
-      onClick={(evt) => {
-        evt.preventDefault();
-        onCityChoose(evt.currentTarget.dataset.city as ChangeCityPayload);
-      }}
-    >
-      <a className="locations__item-link tabs__item" href="/">
-        <span>{cityTitle}</span>
-      </a>
-    </li>
+    <ul className="locations__list tabs__list">
+      {cities.map((city) => (
+        <li
+          key={nanoid(10)}
+          className="locations__item"
+          data-city={city.title}
+          onClick={(evt) => {
+            evt.preventDefault();
+            onCityChoose(evt.currentTarget.dataset.city as ChangeCityPayload);
+          }}
+        >
+          <a className="locations__item-link tabs__item" href="/">
+            <span>{city.title}</span>
+          </a>
+        </li>
+      ))}
+    </ul>
   );
 }
 
