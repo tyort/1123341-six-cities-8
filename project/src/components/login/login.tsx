1c0e3e759b61e0f6ccc2a0d731be0584b2b703bf
tyort <neotyo1986@gmail.com>
2021-10-27 21:13:06 +0300

    Подключение асинхронного действия в login

--- project/src/components/login/login.tsx
+++ project/src/components/login/login.tsx
@@ -1,6 +1,41 @@
+import {useRef, FormEvent} from 'react';
+import {useHistory} from 'react-router-dom';
+import {loginAction} from '../../store/api-actions';
 import Logo from '../logo/logo';
+import {connect, ConnectedProps} from 'react-redux';
+import {ThunkAppDispatch} from '../../types/action';
+import {AuthUserData} from '../../types/auth-user-data';
+import {AppRoute} from '../../const';
 
-function LoginScreen(): JSX.Element {
+const mapDispatchToProps = (dispatch: ThunkAppDispatch) => ({
+  onSubmit(authData: AuthUserData) {
+    // loginAction - асинхронный action
+    dispatch(loginAction(authData));
+  },
+});
+
+const connector = connect(null, mapDispatchToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+function LoginScreen(props: PropsFromRedux): JSX.Element {
+  const {onSubmit} = props;
+
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const history = useHistory();
+
+  const onSubmitHandle = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (emailRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -15,16 +50,40 @@ function LoginScreen(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form
+              className="login__form form"
+              action="#"
+              method="post"
+              onSubmit={onSubmitHandle}
+            >
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <input
+                  ref={emailRef}
+                  className="login__input form__input"
+                  type="email" name="email"
+                  placeholder="Email"
+                  required
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input
+                  ref={passwordRef}
+                  className="login__input form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
               </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
+              <button
+                onClick={() => history.push(AppRoute.Main)}
+                className="login__submit form__submit button"
+                type="submit"
+              >
+                Sign in
+              </button>
             </form>
           </section>
           <section className="locations locations--login locations--current">
@@ -40,4 +99,5 @@ function LoginScreen(): JSX.Element {
   );
 }
 
-export default LoginScreen;
+export {LoginScreen};
+export default connector(LoginScreen);
--- project/src/types/auth-user-data.ts
+++ project/src/types/auth-user-data.ts
@@ -1,4 +1,4 @@
 export type AuthUserData = {
-  login: string;
+  email: string;
   password: string;
 };
