c6d4d50fa9eea18d54dbef31eb43901397b4f98b
tyort <neotyo1986@gmail.com>
2021-10-28 22:12:04 +0300

    Рефакторинг

--- project/src/components/place-offer/place-offer.tsx
+++ project/src/components/place-offer/place-offer.tsx
@@ -87,7 +87,7 @@ function PlaceOfferScreen(props: OfferScreenProps): JSX.Element {
               <h2 className="property__host-title">Meet the host</h2>
               <div className="property__host-user user">
                 <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                  <img className="property__avatar user__avatar" src={`img/${host.avatar_url}`} width="74" height="74" alt="Host avatar"/>
+                  <img className="property__avatar user__avatar" src={`${host.avatar_url}`} width="74" height="74" alt="Host avatar"/>
                 </div>
                 <span className="property__user-name">
                   {host.name}
--- project/src/components/review/review.tsx
+++ project/src/components/review/review.tsx
@@ -1,7 +1,7 @@
-import {Review} from '../../types/offer';
+import {Comment} from '../../types/comment';
 
 type ReviewScreenProps = {
-  review: Review;
+  review: Comment;
 }
 
 function PlaceReviewsScreen(props: ReviewScreenProps): JSX.Element {
@@ -11,21 +11,21 @@ function PlaceReviewsScreen(props: ReviewScreenProps): JSX.Element {
     <li className="reviews__item">
       <div className="reviews__user user">
         <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src={`img/${review.avatar}`} width="54" height="54" alt="Reviews avatar"/>
+          <img className="reviews__avatar user__avatar" src={`${review.user.avatar_url}`} width="54" height="54" alt="Reviews avatar"/>
         </div>
         <span className="reviews__user-name">
-          {review.name}
+          {review.user.name}
         </span>
       </div>
       <div className="reviews__info">
         <div className="reviews__rating rating">
           <div className="reviews__stars rating__stars">
-            <span style={{width: `${review.setRating}%`}}></span>
+            <span style={{width: `${review.rating}%`}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <p className="reviews__text">
-          {review.text}
+          {review.comment}
         </p>
         <time className="reviews__time" dateTime={`${review.date}`}>{review.date}</time>
       </div>
