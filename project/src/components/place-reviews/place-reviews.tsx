3f284c6546f7f415c4ce52e87863019b6cfb0029
tyort <neotyo1986@gmail.com>
2021-10-29 13:43:47 +0300

    place-reviews подключаем к redux

--- project/src/components/place-reviews/place-reviews.tsx
+++ project/src/components/place-reviews/place-reviews.tsx
@@ -1,15 +1,58 @@
+import {connect, ConnectedProps} from 'react-redux';
 import ReviewScreen from '../review/review';
-import {Comment} from '../../types/comment';
+import {Comment, NewComment} from '../../types/comment';
 import {nanoid} from 'nanoid';
 import {AuthorizationStatus} from '../../const';
+import {Fragment, useState, useRef, FormEvent} from 'react';
+import {State} from '../../types/state';
+import {Offer} from '../../types/offer';
+import {ThunkAppDispatch} from '../../types/action';
+import {setCommentAction} from '../../store/api-actions';
+
+
+const STARS_COUNT = 5;
+const starsStatuses = new Map([
+  [1, 'terribly'],
+  [2, 'badly'],
+  [3, 'not bad'],
+  [4, 'good'],
+  [5, 'perfect'],
+]);
 
 type ReviewsScreenProps = {
   comments: Comment[];
-  authorizationStatus: AuthorizationStatus
+  currentOffer: Offer;
 }
 
-function PlaceReviewsScreen(props: ReviewsScreenProps): JSX.Element {
-  const {comments, authorizationStatus} = props;
+const mapStateToProps = (state: State) => ({
+  nearbyOffers: state.nearbyOffers,
+  authorizationStatus: state.authorizationStatus,
+});
+
+const mapDispatchToProps = (dispatch: ThunkAppDispatch) => ({
+  onFucjkas(newComment: NewComment) {
+    dispatch(setCommentAction(newComment));
+  },
+});
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type ConnectedComponentProps = PropsFromRedux & ReviewsScreenProps;
+
+
+function PlaceReviewsScreen(props: ConnectedComponentProps): JSX.Element {
+  const {comments, authorizationStatus, currentOffer, onFucjkas} = props;
+  const stars = new Array(STARS_COUNT).fill('');
+  const [rating, setRating] = useState<number | null>(null);
+  const commentTable = useRef<HTMLTextAreaElement | null>(null);
+
+  const onSubmitHandle = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    if (commentTable.current !== null && rating !== null) {
+      onFucjkas({offerId: currentOffer.id as number, comment: commentTable.current.value, rating});
+    }
+  };
 
   return (
     <section className="property__reviews reviews">
@@ -28,49 +71,35 @@ function PlaceReviewsScreen(props: ReviewsScreenProps): JSX.Element {
         className="reviews__form form"
         action="#"
         method="post"
-        // onSubmit={(evt: FormEvent<HTMLFormElement>) => {
-        //   evt.preventDefault();
-        //   onCommentLoad(offer, userComment);
-        // }}
+        onSubmit={onSubmitHandle}
       >
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
-          <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"/>
-          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
-          <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
-          <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
-          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
-          <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
+          {stars.map((_star, index) => {
+            const realIndex = STARS_COUNT - index;
+            return(
+              <Fragment key={nanoid(10)}>
+                <input
+                  className="form__rating-input visually-hidden"
+                  name="rating"
+                  value={realIndex}
+                  id={`${realIndex}-stars`}
+                  type="radio"
+                  onChange={(evt) => {
+                    setRating(Number(evt.currentTarget.value));
+                  }}
+                />
+                <label htmlFor={`${realIndex}-stars`} className="reviews__rating-label form__rating-label" title={`${starsStatuses.get(realIndex)}`}>
+                  <svg className="form__star-image" width="37" height="33">
+                    <use xlinkHref="#icon-star"></use>
+                  </svg>
+                </label>
+              </Fragment>
+            );
+          })}
         </div>
         <textarea
+          ref={commentTable}
           className="reviews__textarea form__textarea"
           id="review"
           name="review"
@@ -91,4 +120,5 @@ function PlaceReviewsScreen(props: ReviewsScreenProps): JSX.Element {
   );
 }
 
-export default PlaceReviewsScreen;
+export {PlaceReviewsScreen};
+export default connector(PlaceReviewsScreen);
