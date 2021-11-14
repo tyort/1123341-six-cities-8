bea8b74d4132fc844996a1566336d78f73bf1cb9
tyort <neotyo1986@gmail.com>
2021-11-14 12:09:50 +0300

    Рефакторинг компонента favorites

---
+++ project/src/components/favorites-empty/favorites-empty.tsx
@@ -0,0 +1,39 @@
+import HeaderUserScreen from '../header-user/header-user';
+
+function FavoritesScreen(): JSX.Element {
+  return (
+    <div className="page page--favorites-empty">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+              </a>
+            </div>
+            <HeaderUserScreen/>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--favorites page__main--favorites-empty">
+        <div className="page__favorites-container container">
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Favorites (empty)</h1>
+            <div className="favorites__status-wrapper">
+              <b className="favorites__status">Nothing yet saved.</b>
+              <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+            </div>
+          </section>
+        </div>
+      </main>
+      <footer className="footer">
+        <a className="footer__logo-link" href="main.html">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default FavoritesScreen;
