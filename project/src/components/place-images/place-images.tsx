53dd0016d114a13cc02319029b50c0d8c0634eff
tyort <neotyo1986@gmail.com>
2021-11-21 16:23:09 +0300

    Фотографии. Выводится до 6-ти изображений.

--- project/src/components/place-images/place-images.tsx
+++ project/src/components/place-images/place-images.tsx
@@ -9,11 +9,13 @@ function PlaceImagesScreen({images}: PlaceImagesScreenProps): JSX.Element {
   return (
     <div className="property__gallery-container container">
       <div className="property__gallery">
-        {images.map((image) => (
-          <div key={nanoid(10)} className="property__image-wrapper">
-            <img className="property__image" src={`${image}`} alt="View studio"/>
-          </div>
-        ))}
+        {images
+          .slice(0, 6)
+          .map((image) => (
+            <div key={nanoid(10)} className="property__image-wrapper">
+              <img className="property__image" src={`${image}`} alt="View studio"/>
+            </div>
+          ))}
       </div>
     </div>
   );
