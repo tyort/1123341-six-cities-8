fddd368c7b4e027566e9530765377811018a9e61
tyort <neotyo1986@gmail.com>
2022-10-18 20:52:10 +0300

    Страница 404 и индивидуальные стили

---
+++ pages/404.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import Image from 'next/image';
+
+const NotFoundScreen = () => {
+  return (
+    <>
+      <div className="header__wrapper">
+        <div className="header__left">
+          <Link href="#">
+            <a className="header__logo-link header__logo-link--active" style={{pointerEvents: 'none'}}>
+              <Image className="header__logo" src="/img/logo.svg" alt="6 cities logo" width={81} height={41}/>
+            </a>
+          </Link>
+        </div>
+      </div>
+      <h1>404. Fucking page not found</h1>
+      <Link href="/" passHref>
+        <a>Вернуться на главную</a>
+      </Link>
+    </>
+  );
+}
+
+export default NotFoundScreen;
