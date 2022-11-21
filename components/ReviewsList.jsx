f4b4adcbf865142a016600d86ea22b10c962a93a
tyort <neotyo1986@gmail.com>
2022-11-22 00:56:41 +0300

    Создаем новые компоненты

---
+++ components/ReviewsList.jsx
@@ -0,0 +1,11 @@
+import ReviewItemScreen from './ReviewsItem';
+
+function ReviewsListScreen() {
+  return (
+    <ul className='reviews__list'>
+      <ReviewItemScreen />
+    </ul>
+  );
+}
+
+export default ReviewsListScreen;
--- pages/offer/[offerId].js
+++ pages/offer/[offerId].js
@@ -2,6 +2,7 @@ import { Fragment, useState } from 'react';
 import Image from 'next/image';
 import PlacesList from '../../components/PlacesList';
 import OfferRepository from '../../src/repositories/OfferRepository';
+import ReviewsListScreen from '../../components/ReviewsList';
 
 const offerRepository = new OfferRepository();
 
@@ -150,38 +151,7 @@ function OfferScreen({ offer, offers }) {
               <h2 className='reviews__title'>
                 Reviews &middot; <span className='reviews__amount'>1</span>
               </h2>
-              <ul className='reviews__list'>
-                <li className='reviews__item'>
-                  <div className='reviews__user user'>
-                    <div className='reviews__avatar-wrapper user__avatar-wrapper'>
-                      <Image
-                        className='reviews__avatar user__avatar'
-                        src='/img/avatar-max.jpg'
-                        width='54'
-                        height='54'
-                        alt='Reviews avatar'
-                      />
-                    </div>
-                    <span className='reviews__user-name'>Max</span>
-                  </div>
-                  <div className='reviews__info'>
-                    <div className='reviews__rating rating'>
-                      <div className='reviews__stars rating__stars'>
-                        <span style={{ width: '80%' }} />
-                        <span className='visually-hidden'>Rating</span>
-                      </div>
-                    </div>
-                    <p className='reviews__text'>
-                      A quiet cozy and picturesque that hides behind a a river
-                      by the unique lightness of Amsterdam. The building is
-                      green and from 18th century.
-                    </p>
-                    <time className='reviews__time' dateTime='2019-04-24'>
-                      April 2019
-                    </time>
-                  </div>
-                </li>
-              </ul>
+              <ReviewsListScreen />
               <form className='reviews__form form' action='#' method='post'>
                 <label className='reviews__label form__label' htmlFor='review'>
                   Your review
